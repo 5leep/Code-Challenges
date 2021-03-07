e12328a7cbb773047581b3f16a348ad6d6f00e80
Sleep <git.sleep@gmail.com>
2021-03-07 23:55:27 +0000

    Add files via upload

---
+++ 07 03 21/Functionator8000.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Functionator8000
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        String hehe;
+
+        System.out.println("<EV> \n\n\"Please enter a word ;)\"");
+
+        hehe = scan.nextLine();
+        
+        System.out.println("\n\n<EV> \"Allow me to introduce...\"");
+        System.out.println("\n\n<EV> \"The " + hehe + "inator.\"\n\n\n");
+    }
+}
